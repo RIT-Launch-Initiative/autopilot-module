1f83ee40bb933333eeeaceb3b9292377f9745ba5
Aaron Chan <aarchan108@gmail.com>
2023-07-05 19:59:28 -0600

    Just a superloop and event includes for testing

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,3 +1,15 @@
-//
-// Created by aaron on 7/5/23.
-//
+#include "event/ApogeeEvent.h"
+#include "event/BoostEvent.h"
+#include "event/LandingEvent.h"
+#include "event/Event.h"
+#include "return.h"
+#include "sched.h"
+
+
+int main() {
+    while (true) {
+
+    }
+
+    return 0;
+}
